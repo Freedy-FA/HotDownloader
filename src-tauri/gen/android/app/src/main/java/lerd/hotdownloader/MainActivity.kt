91af4a8ae4882532d5b9cc5caf50bbe75d983082
lerdb <lerd233@gmail.com>
2026-08-13 10:23:34 +0800

    feat(android): initialize Android project structure with necessary files
    - Added AndroidManifest.xml for application configuration and permissions.
    - Created MainActivity.kt as the main entry point of the application.
    - Included vector drawable resources for app icons.
    - Defined layout for the main activity with a simple TextView.
    - Added mipmap resources for different screen densities.
    - Created XML files for colors, strings, and themes.
    - Configured Gradle build files and wrapper for project setup.
    - Implemented custom Gradle tasks for building Rust libraries.

---
+++ src-tauri/gen/android/app/src/main/java/lerd/hotdownloader/MainActivity.kt
@@ -0,0 +1,11 @@
+package lerd.hotdownloader
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
